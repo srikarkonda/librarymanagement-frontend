29778e09c5b2568f018de7a0878678d008cf12f9
srikarkonda <43203729+srikarkonda@users.noreply.github.com>
2023-08-25 14:49:56 -0500

    Update apis.service.ts

--- src/app/apis.service.ts
+++ src/app/apis.service.ts
@@ -11,73 +11,73 @@ export class ApisService {
 
   login(userDetails:any)
   {
-    return this.http.post('http://localhost:3000/api/auth/login',userDetails)
+    return this.http.post('https://librarymanagement-backend-1e5x.onrender.com/api/auth/login',userDetails)
   }
 
   getProducts()
   {
     const userData = JSON.parse(sessionStorage.getItem('user') || '{}');
-    return this.http.get('http://localhost:3000/api/product/getBuyerProducts',{headers:{'userId': userData[0]?.userId}})
+    return this.http.get('https://librarymanagement-backend-1e5x.onrender.com/api/product/getBuyerProducts',{headers:{'userId': userData[0]?.userId}})
   }
   getSellerProducts()
   {
     const userData = JSON.parse(sessionStorage.getItem('user') || '{}');
-    return this.http.get('http://localhost:3000/api/product/getSellerProducts',{headers:{'userId': userData[0]?.userId}})
+    return this.http.get('https://librarymanagement-backend-1e5x.onrender.com/api/product/getSellerProducts',{headers:{'userId': userData[0]?.userId}})
   }
 
   addProduct(body:any)
   {
-    return this.http.post('http://localhost:3000/api/product/addProducts',body)
+    return this.http.post('https://librarymanagement-backend-1e5x.onrender.com/api/product/addProducts',body)
   }
 
   updateProduct(body:any)
   {
     console.log(body)
-    return this.http.post('http://localhost:3000/api/product/updateProducts',body)
+    return this.http.post('https://librarymanagement-backend-1e5x.onrender.com/api/product/updateProducts',body)
   }
 
   checkoutOrder(body:any)
   {
-    return this.http.post('http://localhost:3000/api/product/checkout',body)
+    return this.http.post('https://librarymanagement-backend-1e5x.onrender.com/api/product/checkout',body)
   }
 
   getLibraryBooks()
   {
-    return this.http.get('http://localhost:3000/getBooks')
+    return this.http.get('https://librarymanagement-backend-1e5x.onrender.com/getBooks')
   }
 
   saveChanges(body:any)
   {
-    return this.http.post('http://localhost:3000/saveChanges',body)
+    return this.http.post('https://librarymanagement-backend-1e5x.onrender.com/saveChanges',body)
   }
   deleteBook(ids:any)
   {
-    return this.http.delete('http://localhost:3000/deleteSelectedBooks',{params:{'ids': ids}})
+    return this.http.delete('https://librarymanagement-backend-1e5x.onrender.com/deleteSelectedBooks',{params:{'ids': ids}})
   }
 
   getLoanRecords()
   {
-    return this.http.get('http://localhost:3000/getLoanRecords')
+    return this.http.get('https://librarymanagement-backend-1e5x.onrender.com/getLoanRecords')
   }
 
   insertLoanData(data:any)
   {
-    return this.http.post('http://localhost:3000/saveLoanRecords',data)
+    return this.http.post('https://librarymanagement-backend-1e5x.onrender.com/saveLoanRecords',data)
   }
 
   updateLoanData(data:any)
   {
-    return this.http.post('http://localhost:3000/returnBooks',data)
+    return this.http.post('https://librarymanagement-backend-1e5x.onrender.com/returnBooks',data)
   }
 
   search(search:any)
   {
-    return this.http.get('http://localhost:3000/searchResults/'+search)
+    return this.http.get('https://librarymanagement-backend-1e5x.onrender.com/searchResults/'+search)
   }
 
   getMyLoanRecords()
   {
     const userData = JSON.parse(sessionStorage.getItem('user') || '{}');
-    return this.http.get('http://localhost:3000/getMyLoanRecords',{headers:{'userId': userData[0]?.userId}})
+    return this.http.get('https://librarymanagement-backend-1e5x.onrender.com/getMyLoanRecords',{headers:{'userId': userData[0]?.userId}})
   }
 }
